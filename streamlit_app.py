169916aaa30d764c9a9c918d7fd224a06d7e0add
uotou <terenceou@yahoo.com>
2022-11-26 16:47:17 +0800

    added line to use streamlit.multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,17 @@
-import streamlit
+import streamlit as sl
+
+sl.title('My Parents New Healthy Diner')
+sl.header('Breakfast Menu')
+
+sl.text('🥣 Omega 3 & Blueberry Oatmeal')
+sl.text('🥗 Kale, spinach & Rocket Smoothie')
+sl.text('🐔 Hard-Boiled Free-Range Egg')
+sl.text('🥑🍞 Avocado Toast')
+
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 import pandas as pd
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+sl.multiselect("Pick some fruits:", list(my_fruit_list))
+sl.dataframe(my_fruit_list)
