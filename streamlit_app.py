38046b5ea5e09e1d4e335895e521e98e6c9ef96c
uotou <terenceou@yahoo.com>
2022-11-26 16:51:54 +0800

    added default selectionfor multiselect

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ import pandas as pd
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-sl.multiselect("Pick some fruits:", list(my_fruit_list.index))
+sl.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 sl.dataframe(my_fruit_list)
