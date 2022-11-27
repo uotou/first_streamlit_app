1e4fa45fc854497ff3528ffec3728585eb57bdaf
uotou <terenceou@yahoo.com>
2022-11-27 18:44:15 +0800

    definced a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 sl.dataframe(fruits_to_show)
 
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 
 sl.header('Fruityvice Fruit Advice!')
 try:
@@ -32,9 +37,8 @@ try:
     if not fruit_choice:
         sl.error("Please select a fruit to get information")
     else:
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-        sl.dataframe(fruityvice_normalized)
+        back_from_function = get_fruityvice_data(fruit_choice)
+        sl.dataframe(back_from_function)
 
 except URLError as e:
         sl.error()
