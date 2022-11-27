4b2dd1b10cabae0efa5e1c3f6faec044673bb762
uotou <terenceou@yahoo.com>
2022-11-27 17:23:38 +0800

    added text input

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ sl.dataframe(fruits_to_show)
 
 
 sl.header('Fruityvice Fruit Advice!')
+fruit_choice = sl.text_input('What fruit would you like information about?','Kiwi')
+sl.write('The user entered ', fruit_choice)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
