41c2658744b754dac6fc4a8811f5a576b3dc16c8
uotou <terenceou@yahoo.com>
2022-11-27 18:23:38 +0800

    fetch all instead of one

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 sl.header("The fruit load list contains:")
-sl.dataframe(my_data_row)
+sl.dataframe(my_data_rows)
