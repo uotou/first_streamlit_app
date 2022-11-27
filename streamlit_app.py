f260d2bf0d77d6b96c4e32f4d3393306b0f9e2ee
uotou <terenceou@yahoo.com>
2022-11-27 18:22:06 +0800

    changed sql code to retrieve data from fruit_load_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
-sl.text("Hello from Snowflake:")
+sl.text("The fruit load list contains:")
 sl.text(my_data_row)
