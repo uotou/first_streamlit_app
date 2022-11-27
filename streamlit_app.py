5b89f4a18b44cade2326a0d6bad8df260ce4bc8e
uotou <terenceou@yahoo.com>
2022-11-27 18:28:47 +0800

    added default value for the textinput for add_my_fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ my_data_rows = my_cur.fetchall()
 sl.header("The fruit load list contains:")
 sl.dataframe(my_data_rows)
 
-add_my_fruit = sl.textinput("What fruit would you like to add?")
-sl.text('Thanks for adding' + add_my_fruit)
+add_my_fruit = sl.textinput("What fruit would you like to add?", 'jackfruit')
+sl.text('Thanks for adding ' + add_my_fruit)
