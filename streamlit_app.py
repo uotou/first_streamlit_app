5d43c608d190f950302c3f43d8a81fc1f31c67f1
uotou <terenceou@yahoo.com>
2022-11-27 18:18:39 +0800

    added connection to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,10 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 sl.dataframe(fruityvice_normalized)
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+sl.text("Hello from Snowflake:")
+sl.text(my_data_row)
