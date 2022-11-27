cc122b524cfd6538338bf7ac929dd423f3926499
uotou <terenceou@yahoo.com>
2022-11-27 17:24:41 +0800

    using fruit_choice as the parameter for the request

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruit_choice = sl.text_input('What fruit would you like information about?','Kiw
 sl.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # convert from semi-structured to structured
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
