c4cfc20c5d61c8dc708e6e0437bd76e4b1caa0d3
uotou <terenceou@yahoo.com>
2022-11-27 18:29:35 +0800

    corrected textinput to text_input

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,5 +44,5 @@ my_data_rows = my_cur.fetchall()
 sl.header("The fruit load list contains:")
 sl.dataframe(my_data_rows)
 
-add_my_fruit = sl.textinput("What fruit would you like to add?", 'jackfruit')
+add_my_fruit = sl.text_input('What fruit would you like to add?', 'jackfruit')
 sl.text('Thanks for adding ' + add_my_fruit)
